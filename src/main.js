826ef90187acf312ece3fd89a815cbff74916725
CCQQ1 <630688213@qq.com>
2019-11-27 16:20:50 +0800

    整合element-ui

--- src/main.js
+++ src/main.js
@@ -4,6 +4,20 @@ import App from './App.vue'
 // 导入抽取的路由
 import router from './router/router.js';
 
+// 导入Element - ui 
+import ElementUI from 'element-ui';
+// 导入Element -ui的样式
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 导入自己的全局样式
+import './style/base.css'
+
+// 使用element- ui   use一下
+Vue.use(ElementUI)
+
+
+
+// 控制台打印信息
 Vue.config.productionTip = false
 
 new Vue({
